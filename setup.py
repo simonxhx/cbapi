3f60499209ccc17610ae53d5c5290c4b139004de
simonxhx <68516486+simonxhx@users.noreply.github.com>
2020-08-15 11:38:28 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,11 @@
 from setuptools import setup
 
 setup(
-    # Needed to silence warnings (and to be a worthwhile package)
     name='cbapi',
     url='https://github.com/simonxhx/cbapi',
     author='Simon Xiong',
     author_email='simon.xiong.baruchmfe@gmail.com',
-    # Needed to actually package something
     packages=['cbapi'],
-    # Needed for dependencies
     install_requires=['json', 'requests', 'pandas', 'os', 'threading'],
     version='1.0.0',
     description='An API library for downloading organization and people data from Crunchbase',
